0b7f329eab90778cfd57a91f4efa438f0ae2bef0
dpp17 <durga123q.com>
2023-05-16 23:28:27 +0530

    Added:Launched Bridgelabz's site in a new tab on clicking Bridgelabz Logo

--- src/App.js
+++ src/App.js
@@ -8,20 +8,47 @@ function ComponentStore(){
   );
 }
 
+function ComponentOnClickBridgelabzLogo(){
+  const urlComponent = "https://www.bridgelabz.com/";
+      return ()=>{
+        window.open(urlComponent,'_blank')
+      };
+}
+
 // "export default" is used to tell the main component 
 export default function App() {
   const displayString = "Hello from BridgeLabz";
+  const url = "https://www.bridgelabz.com/";
+
+  // Two ways to open website::
+  // 1. by creating functions
+  // 2. by creating component 
+
+  // Method:: 01
+  let onClickBridgelabzLogo = ()=>{
+    window.open(url,'_blank');
+  };
+
+  // Method:: 02
+  // const onClickBridgelabzLogoByCompnent = ComponentOnClickBridgelabzLogo();
+
   return (
     <>
 
-    {/* this is one way binding */}
-    <h1>{displayString}</h1>
+        {/* this is one way binding */}
+        <h1>{displayString}</h1>
 
-    {/* this way we can use other components */}
-    <ComponentStore />
+        {/* this way we can use other components */}
+        <ComponentStore />
 
-    {/* this way we can use Property-Binding */}
-    <img src={logo} alt='Bridgelabz logo not found'/>
+        {/* ==>> " This way we can use Property-Binding " */}
+
+        {/* Method::01 for open website {reference line :: 31} */}
+        <img src={logo} alt='Bridgelabz logo not found' onClick={onClickBridgelabzLogo} />
+        
+        {/* Method::02 for open website {reference line :: 11,32} */}
+        {/* <img src={logo} alt='Bridgelabz logo not found' onClick={onClickBridgelabzLogoByCompnent} /> */}
+
     </>
 
   );
